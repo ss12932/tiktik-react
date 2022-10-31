1aaff585b32c675132f34d7a046b410c325a7e87
ss12932 <ss12932@my.bristol.ac.uk>
2022-10-31 22:00:41 +0000

    basic footer added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { footerList1, footerList2, footerList3 } from '../utils/constants';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="mt-6 hidden xl:block">
+      <div className="flex flex-wrap gap-2 mt-5">
+        {footerList1.map((item) => (
+          <p
+            key={item}
+            className="text-gray-400 text-sm hover:underline cursor-pointer"
+          >
+            {item}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
