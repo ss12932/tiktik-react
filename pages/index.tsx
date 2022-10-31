5e98ea7aee6cd7825d4321b7cd092feaf7e91846
ss12932 <ss12932@my.bristol.ac.uk>
2022-10-31 14:58:38 +0000

    added logo + import utils

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return (
-    <h1 className="text-5xl font-bold underline bg-red-500">Hello world!</h1>
-  );
+  return <h1 className="text-5xl font-bold underline">Hello world!</h1>;
 };
 
 export default Home;
