1abcc763b7f93ce6d1e4d288fbdf5b3202700425
ss12932 <ss12932@my.bristol.ac.uk>
2022-11-14 14:27:52 +0000

    added types dec ts file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
 import type { NextPage } from 'next';
+import axios from 'axios';
 
-const Home: NextPage = () => {
+// interface IProps {
+//   videos:
+// }
+
+const Home: NextPage = ({ videos }) => {
+  console.log(videos);
   return <h1 className="text-5xl font-bold underline">Hello world!</h1>;
+};
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(`http://localhost:3000/api/post`);
+
+  return {
+    props: { videos: data },
+  };
 };
 
 export default Home;
