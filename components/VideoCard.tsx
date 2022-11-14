c3751f35067557f68ec0e1d80e7bc1aebfb74506
ss12932 <ss12932@my.bristol.ac.uk>
2022-11-14 21:55:57 +0000

    initial video card component styled + states

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,12 +1,91 @@
-import React from 'react';
-import { Video } from '../types.d';
+import React, { useState, useEffect, useRef } from 'react';
 import { NextPage } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
+import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
+import { BsPlay, BsFillPlayFill, BsFillPauseFill } from 'react-icons/bs';
+import { GoVerified } from 'react-icons/go';
+import { Video } from '../types.d';
 
 interface IProps {
   post: Video;
 }
 const VideoCard: NextPage<IProps> = ({ post }) => {
-  return <div>VideoCard</div>;
+  const [isHover, setIsHover] = useState(false);
+  const [playing, setPlaying] = useState(false);
+  const [isVideoMuted, setIsVideoMuted] = useState(false);
+  return (
+    <div className="flex flex-col border-b-2 border-gray-200 pb-6">
+      <div>
+        <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded">
+          <div className="md:w-16 md:h-16 w-10 h-10">
+            <Link href="/">
+              <>
+                <Image
+                  width={62}
+                  height={62}
+                  className="rounded-full"
+                  src={post.postedBy.image}
+                  alt="profile shoot"
+                  layout="responsive"
+                />
+              </>
+            </Link>
+          </div>
+          <div>
+            <Link href="/">
+              <div className="flex iterms-center gap-2">
+                <p className="flex gap-2 items-center md:text-md font-bold text-primary">
+                  {post.postedBy.userName}
+                  <GoVerified className="text-blue-400 text-md" />
+                </p>
+                <p className="capitalize font-medium text-xs text-gray-500 hidden md:block">
+                  {post.postedBy.userName}
+                </p>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="lg:mll-20 flex gap-4 relative">
+        <div
+          onMouseEnter={() => setIsHover(true)}
+          onMouseLeave={() => setIsHover(false)}
+          className="rounded-3xl"
+        >
+          <Link href="/">
+            <video
+              loop
+              className="lg:w-[600px] h-[300px] md:h-[400px] lg:h-[530px] rounded-2xl cursor-pointer bg-gray-100"
+              src={post.video?.asset.url}
+            ></video>
+          </Link>
+          {isHover && (
+            <div>
+              {playing ? (
+                <button>
+                  <BsFillPauseFill className="text-black text-2xl lg:text-4xl" />
+                </button>
+              ) : (
+                <button>
+                  <BsFillPlayFill className="text-black text-2xl lg:text-4xl" />
+                </button>
+              )}
+              {isVideoMuted ? (
+                <button>
+                  <HiVolumeOff className="text-black text-2xl lg:text-4xl" />
+                </button>
+              ) : (
+                <button>
+                  <HiVolumeUp className="text-black text-2xl lg:text-4xl" />
+                </button>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default VideoCard;
--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['avatars.githubusercontent.com'],
+  },
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
