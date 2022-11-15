0699c96827f8482710e70ccb76b5b520c557068d
ss12932 <ss12932@my.bristol.ac.uk>
2022-11-15 15:24:55 +0000

    implement google oauth + video ctrls for videocard

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { GoogleLogin, GoogleLogout } from 'react-google-login';
 import { AiOutlineLogout } from 'react-icons/ai';
+import { GoogleLogin, googleLogout } from '@react-oauth/google';
 import { BiSearch } from 'react-icons/bi';
 import { IoMdAdd } from 'react-icons/io';
 
 import Logo from '../utils/tiktik-logo.png';
 
 const Navbar = () => {
+  const user = false;
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
@@ -22,6 +23,8 @@ const Navbar = () => {
           />
         </div>
       </Link>
+      <div>SEARCH</div>
+      <div>{user ? <div>Loggede In</div> : <GoogleLogin />}</div>
     </div>
   );
 };
